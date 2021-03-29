3684e361b376d1687a6bc6a2ab5ea18b1d88f1f5
saor-rocmen <sergey.sergeich021@gmail.com>
2021-03-29 22:29:21 +0600

    готовы и адаптированны табы и коментарии слйдер под вопросом адаптации

--- src/js/script.js
+++ src/js/script.js
@@ -2,16 +2,38 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1000,
       //  adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow_left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow_right.png"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.png"></button>',
         responsive:[
             {
                 breakpoint: 992,
                 settings: {
-                  dots: true,
+                  dots: false,
                   arrows: false
                 }
               }
         ]
       });
+
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.contayner').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      }); 
+
+      $('.catalog-item__link').each(function(i) {
+        $(this).on('click', function(e) {
+          e.preventDefault();
+          $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+          $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+      });
+
+      $('.catalog-item__back').each(function(i) {
+        $(this).on('click', function(e) {
+          e.preventDefault();
+          $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+          $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+      })
   });
